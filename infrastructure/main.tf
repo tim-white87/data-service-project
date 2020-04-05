98795328ac7bf9d9e0c538bfc3873996faa65a8d
Tim White <timma.j.white@gmail.com>
2020-04-05 13:54:46 -0600

    setup a cognito user pool for auth

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,3 +9,23 @@ resource "aws_s3_bucket" "data-service-project-bucket" {
     enabled = true
   }
 }
+
+resource "aws_cognito_user_pool" "data-service-project-pool" {
+  name = "data-service-project"
+}
+
+resource "aws_cognito_user_pool_client" "data-service-project-client" {
+  name = "data-service-project-client"
+
+  user_pool_id = aws_cognito_user_pool.data-service-project-pool.id
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows = ["code"]
+  callback_urls = ["https://localhost:5001/signin-oidc"]
+  allowed_oauth_scopes = ["email", "openid", "profile"]
+  supported_identity_providers  = ["COGNITO"]
+}
+
+resource "aws_cognito_user_pool_domain" "main" {
+  domain       = "data-service-project"
+  user_pool_id = aws_cognito_user_pool.data-service-project-pool.id
+}
