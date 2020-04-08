dea5b76a1a99dce18b97025ad221a12e0ef6dffa
Tim White <timma.j.white@gmail.com>
2020-04-07 22:24:38 -0600

    fixed login issue

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -109,7 +109,7 @@ resource "aws_cognito_user_pool_client" "data-service-project-client" {
   user_pool_id                         = aws_cognito_user_pool.data-service-project-pool.id
   allowed_oauth_flows_user_pool_client = true
   allowed_oauth_flows                  = ["code", "implicit"]
-  callback_urls                        = ["http://localhost:3000/", "https://${aws_cloudfront_distribution.s3_distribution.domain_name}"]
+  callback_urls                        = ["http://localhost:3000/", "https://${aws_cloudfront_distribution.s3_distribution.domain_name}/"]
   allowed_oauth_scopes                 = ["email", "openid", "profile"]
   supported_identity_providers         = ["COGNITO"]
 }
